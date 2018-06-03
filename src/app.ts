64b6f3dd42f4d56d1f38151dc98c2689760f7b56
Jack <jisapp02@gmail.com>
2018-06-02 22:06:38 -0400

    typescript

--- src/app.ts
+++ src/app.ts
@@ -1,7 +1,7 @@
-const express = require('express');
+import * as express from 'express';
 const app = express();
 
-app.use(express.static('client'))
+app.use(express.static(__dirname + '/client'))
 app.get('/', (req, res) => res.send('Hello World!'))
 
 app.listen(3000, () => console.log('Example app listening on port 3000!'))
