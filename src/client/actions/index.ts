975988cb57627f733fca84f0561ba396352bb65d
Jack <jisapp02@gmail.com>
2018-06-24 15:26:36 -0400

    Implement thunk WIP

--- src/client/actions/index.ts
+++ src/client/actions/index.ts
@@ -1,7 +1,21 @@
+// export function getPlaylists (ytProxy) {
+//   console.log('~= DISPATCHING ACTION')
+//   return {
+//     type: 'GET_PLAYLISTS',
+//     payload: ytProxy.getPlaylists(),
+//   }
+// }
+
 export function getPlaylists (ytProxy) {
   console.log('~= DISPATCHING ACTION')
-  return {
-    type: 'GET_PLAYLISTS',
-    payload: ytProxy.getPlaylists(),
+  return function (dispatch) {
+    console.log('~= THUNKIN')
+    return ytProxy.getPlaylists()
+      .then(playlists => {
+        dispatch({
+          type: 'GET_PLAYLISTS',
+          payload: playlists,
+        })
+      })
   }
 }
--- src/client/index.tsx
+++ src/client/index.tsx
@@ -2,14 +2,21 @@ import * as React from 'react';
 import * as ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
 import { createStore, applyMiddleware } from 'redux';
-import { devToolsEnhancer, composeWithDevTools } from 'redux-devtools-extension';
+import thunkMiddleware from 'redux-thunk';
+import { createLogger } from 'redux-logger'
+import { composeWithDevTools } from 'redux-devtools-extension';
 import App from './components/App';
 import rootReducer from './reducers';
 
+const loggerMiddleware = createLogger();
+
 const store = createStore(
   rootReducer,
-  // devToolsEnhancer({}),
-  composeWithDevTools()
+  composeWithDevTools(
+    applyMiddleware(
+      thunkMiddleware,
+      // loggerMiddleware
+    ))
 );
 
 ReactDOM.render(
